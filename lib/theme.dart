9c2041a4985a69d485c252db7c211e7079546f47
ITEASY <almuhambetoveset@gmail.com>
2026-02-18 15:18:40 +0500

    112

---
+++ lib/theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+ThemeData LightTheme = ThemeData(
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.brown,
+      brightness: Brightness.light,
+    )
+);
+
+ThemeData DarkTheme = ThemeData(
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.brown,
+      brightness: Brightness.dark,
+    )
+);
