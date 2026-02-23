c9764f3ef212978c70dbabb83fbc0492bd604e44
esit1212 <almuhambetoveset@gmail.com>
2026-02-23 21:49:43 +0500

    1

--- lib/pages/mainscreen.dart
+++ lib/pages/mainscreen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'todolist.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import '../../theme.dart';
 
 class Mainscreen extends StatefulWidget {
   const Mainscreen({
